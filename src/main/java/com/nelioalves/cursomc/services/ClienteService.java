67052a582210599da20749324dac93a7e98e5cce
Matheus Barbosa <matheusb565@gmail.com>
2019-08-28 14:25:40 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -124,7 +124,22 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multiPartFile) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		URI uri = s3Service.uploadFile(multiPartFile);
+		
+		Optional<Cliente> cli = repo.findById(user.getId());
+		cli.get().setImageUrl(uri.toString());
+		repo.save(cli.get());
+		
 		return s3Service.uploadFile(multiPartFile);
 	}
+	
+	
 	
 }
